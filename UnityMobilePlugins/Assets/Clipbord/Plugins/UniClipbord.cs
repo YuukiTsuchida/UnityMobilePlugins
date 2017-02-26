b637d2fcc34dfdcd893936620d3b7c210c4d46b0
Yuuki Tsuchida <dev.yuukitsuchida@gmail.com>
2017-02-27 00:20:41 +0900

    Andoridのクリップボード機能を追加

---
+++ UnityMobilePlugins/Assets/Clipbord/Plugins/UniClipbord.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace NativeSystem
+{
+    public static class UniClipbord 
+    {
+        public static void Copy(string text)
+        {
+    #if !UNITY_EDITOR && UNITY_IOS
+    #elif !UNITY_EDITOR && UNITY_ANDROID
+            AndroidJavaClass unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+            AndroidJavaObject activity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+
+            activity.Call("runOnUiThread", new AndroidJavaRunnable(() =>
+                        {
+                            AndroidJavaObject clipboardManager = activity.Call<AndroidJavaObject>("getSystemService","clipboard");
+                            AndroidJavaClass clipDataClass = new AndroidJavaClass("android.content.ClipData");
+                            AndroidJavaObject clipData = clipDataClass.CallStatic<AndroidJavaObject>("newPlainText","simple text", text);
+                            clipboardManager.Call("setPrimaryClip", clipData);
+                        }));
+    #else
+            GUIUtility.systemCopyBuffer = text;
+    #endif
+        }
+    }
+}
---
+++ UnityMobilePlugins/Assets/Clipbord/Test/ClipbordTest.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClipbordTest : MonoBehaviour 
+{
+    public void OnPushButton(string text)
+    {
+        NativeSystem.UniClipbord.Copy(text);
+    }
+}
